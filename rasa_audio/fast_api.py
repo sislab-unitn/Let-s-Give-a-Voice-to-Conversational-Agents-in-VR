9f8b27c6b3b4660e1560db2a3911129852d9d56b
Michele Yin <mickyin3@gmail.com>
2023-02-11 09:48:10 +0100

    added Unity empty scene with git ignore

--- rasa_audio/fast_api.py
+++ rasa_audio/fast_api.py
@@ -12,41 +12,15 @@ app = FastAPI()
 
 token_auth_scheme = HTTPBearer()
 
-# class Item(BaseModel):
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/message")
+@app.get("/text_converse")
 def message(q:str,v:str ="20221114",token: str = Depends(token_auth_scheme)):
     r = requests.get(url = wit_URL+'message', params = {"q":q,"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
     return r.json()
-@app.get("/language")
+@app.get("/audio_converse")
 def language(q:str,v:str ="20221114",token: str = Depends(token_auth_scheme)):
     r = requests.get(url = wit_URL+'language', params = {"q":q,"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/intents")
-def intents(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URL+'intents', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/entities")
-def entities(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URLE+'entities', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/traits")
-def traits(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URL+'traits', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/utterances")
-def utterances(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URL+'utterances', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/app")
-def get_app(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URL+'app', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
-    return r.json()
-@app.get("/voices")
-def voices(v:str ="20221114",token: str = Depends(token_auth_scheme)):
-    r = requests.get(url = wit_URL+'voices', params = {"v":v},headers={'Authorization': 'Bearer ' + token.credentials})
     return r.json()
