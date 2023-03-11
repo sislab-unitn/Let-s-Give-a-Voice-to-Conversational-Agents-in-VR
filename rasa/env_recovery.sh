698f2f4d7840a377a1a71adf9cdfb8cec6e3f11c
Michele Yin <mickyin3@gmail.com>
2023-03-11 15:39:50 +0100

    removed old version of rasa bot

--- rasa/env_recovery.sh
+++
@@ -1,7 +0,0 @@
-conda env remove --name rasa
-conda create --name rasa python=3.9
-conda activate rasa
-pip install rasa
-pip install tmdbsimple
-
-# remember to activate the environment and be in the correct rasa folder
