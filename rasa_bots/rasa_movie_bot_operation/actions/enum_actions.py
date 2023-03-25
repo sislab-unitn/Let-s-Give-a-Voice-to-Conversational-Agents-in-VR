01ac007fe057edfb8e267e4a8d1168f380c9b37f
Michele Yin <mickyin3@gmail.com>
2023-03-25 14:45:52 +0100

    cleanup

--- rasa_bots/rasa_movie_bot_operation/actions/enum_actions.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Actions(Enum):
-    ActionDiscoverMovie = "action_discover_movie"
-    ActionGenresAvailable = "action_genres_available"
-    ValidateMovieTvForm = "validate_movie_tv_form"
-    ValidateMovieTvGenreForm = "validate_movie_tv_genre_form"
