fb8c3093606b9e7a1941680872abdb55f45bd101
Michele Yin <mickyin3@gmail.com>
2023-03-27 12:01:21 +0200

    first demo of triage and anamnesi

---
+++ rasa_bots/rasa_anamnesi/actions/__init__.py
@@ -0,0 +1,19 @@
+# __title__ = "tmdbactionbot"
+# __version__ = "0.1.1"
+# __author__ = "Michele Yin"
+# __copyright__ = "Copyright (c) 2022-2023 Michele Yin"
+# __license__ = "GPLv3"
+
+# # import os
+# # import requests
+
+# __all__ = [
+#     "action_discover_movies.py",
+#     "action_genres_available.py",
+#     "action_movie_tv_form_validation.py",
+#     "action_movie_tv_genre_form_validation.py",
+#     "action_query_knowledge_base.py",
+#     "config_parser.py",
+#     "enum_actions.py",
+#     "enum_slots.py",
+# ]
