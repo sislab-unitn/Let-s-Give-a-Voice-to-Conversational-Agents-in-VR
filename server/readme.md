10d4e93cec30fd8fa20853e2634f9a492c52c64f
Michele Yin <mickyin3@gmail.com>
2023-04-12 17:01:08 +0200

    deletion of useless stuff

--- server/readme.md
+++ server/readme.md
@@ -1,7 +1,12 @@
 # server
 
-Install the requirements from the `requirements.txt` file
+Install the requirements from the `requirements.txt` file or create an environment with the bundled `environment.yml` file
 
 configure the `config.toml` file using the correct port and address you want for this server and the other asr and tts servers
 
 run using `python server.py`
+
+by default the rasa servers are not run automatically, therefore you should start them manually
+
+
+You can debug by running `python text_talker.py` as it provides the same interface as the standard server, but without `ASR` and `TTS`, by allowing a text based conversation.
--- server/text_talker.py
+++ server/text_talker.py
@@ -1,9 +1,13 @@
+'''
+simple interface to talk to a bot via the command line by sending a post request to the server
+Useful for debugging and testing with a fixed sender_id in order to track the conversation
+'''
 import requests
 import sys
 import os
 from argparse import ArgumentParser
 args = ArgumentParser(description="Talk to a bot via the command line")
-args.add_argument("--bot", help="The bot to talk to", default="triage_bot")
+args.add_argument("--bot", help="The bot to talk to", required=True)
 args.add_argument("--url", help="The url to talk to", default="http://localhost:8080/text_converse")
 args.add_argument("--sender", help="The sender id", default="test")
 if __name__ == "__main__":
